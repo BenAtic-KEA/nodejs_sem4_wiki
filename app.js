4354da50501782e24c6bb21fc97a533f94bec040
Mark Hansen <Mark024e@stud.kea.dk>
2022-10-18 13:36:37 +0200

    frontpage initial design done

--- app.js
+++ app.js
@@ -7,11 +7,61 @@ app.use(express.static("public"))
 
 // app.use(<routername>) for routers
 
-const frontpagePage = renderPage("/frontpage/frontpage.html", {tabTitle : "NodeJs helper", cssLink : ""});
+//Frontpage
+const frontpagePage = renderPage("/frontpage/frontpage.html", {tabTitle : "NodeJs helper", cssLink : "pages/frontpage/frontpage.css"});
+
+//Login
+const loginPage = renderPage("/loginpage/login.html", {tabTitle: "Login / Signup", cssLink: ""});
+
+// NPM tabs:
+const projectSetupPage = renderPage("/setup_pages/projectSetup/projectSetup.html", {tabTitle: "Project setup", cssLink:""});
+const terminalCommandsPage = renderPage("/setup_pages/terminalCommands/terminalCommands.html", {tabTitle: "Terminal Commands", cssLink:""});
+const toolsPage = renderPage("/setup_pages/tools/tools.html", {tabTitle: "NPM tools", cssLink:""});
+
+// Javascript tabs:
+const fetchAndRedirectPage = renderPage("/javascriptTopics/FetchAndRedirect/fetchAndRedirect.html",{tabTitle: "Fetch & Redirect", cssLink: ""});
+const variablesAndFunctionsPage = renderPage("/javascriptTopics/variablesAndFunctions/variablesAndFunctions.html", {tabTitle: "Variables & Fuctiosn", cssLink:""});
+const restAPIPage = renderPage("/javascriptTopics/RestAPI/restAPI.html", {tabTitle: "REST API", cssLink:""});
+const SSRandCSRPage = renderPage("/javascriptTopics/SSRAndCSR/SSRAndCSR.html", {tabTitle: "Server & Client side Rendering", cssLink:""});
+
 
 app.get("/",(req,res) => {
-    res.send(frontpagePage)
-})
+    res.send(frontpagePage);
+});
+
+app.get("/login", (req,res) =>{
+    res.send(loginPage);
+});
+
+app.get("/fetchAndRedirect", (req,res) =>{
+    res.send(fetchAndRedirectPage);
+});
+
+app.get("/variablesAndFunctions", (req,res) => {
+    res.send(variablesAndFunctionsPage);
+});
+
+app.get("/RestAPI", (req,res) => {
+    res.send(restAPIPage);
+});
+
+app.get("/SSR-CSR", (req,res) => {
+    res.send(SSRandCSRPage);
+});
+
+app.get("/projectSetup", (req,res) => {
+    res.send(projectSetupPage);
+});
+
+app.get("/terminal-commands", (req,res) => {
+    res.send(terminalCommandsPage);
+});
+
+app.get("/npm-tools", (req,res) => {
+    res.send(toolsPage);
+});
+
+
 
 
 
